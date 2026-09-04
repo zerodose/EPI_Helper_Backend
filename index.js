57492661adec6227bb0a63a98386d2bb25b75626
zerodose <zerodosechild@gmail.com>
2026-09-04 17:25:36 +0500

    Update index.js index dns

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import "dotenv/config";
+
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
+
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
